f70856857ea8e81eb20ac1fb5a5d50c31898ec23
lodepuly <admin@qq.com>
2023-03-01 20:52:30 +0800

    更新BingAI

--- Resource/Script/Bing/BingAI.js
+++ Resource/Script/Bing/BingAI.js
@@ -10,8 +10,8 @@
    
 
    headers['User-Agent'] = `Mozilla/5.0 (${ua}; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41`;
-  headers['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"'
-  headers['sec-ch-ua-mobile'] = '?0'
-  headers['sec-ch-ua-platform'] = 'macOS'
+   headers['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"'
+   headers['sec-ch-ua-mobile'] = '?0'
+   headers['sec-ch-ua-platform'] = 'macOS'
 
    $done({ headers });
