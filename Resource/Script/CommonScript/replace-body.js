a6b07dd3f487aa920025b696230984754c590ac1
lodepuly <admin@qq.com>
2023-03-30 13:58:41 +0800

    更新通用脚本

--- Resource/Script/CommonScript/replace-body.js
+++ Resource/Script/CommonScript/replace-body.js
@@ -39,9 +39,9 @@ let body;
 if (typeof $argument == "undefined") {
 	console.log("requires $argument");
 } else {
-    if ($response != undefined) {
+    if (typeof $response != "undefined") {
 		body = $response.body;
-	} else if ($request != undefined) {
+	} else if (typeof $request != "undefined") {
 		body = $request.body;
 	} else {
 		console.log("script type error");
