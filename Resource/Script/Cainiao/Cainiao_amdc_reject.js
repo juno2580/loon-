607013b22f2242c5306bf0dbfbf2871654cd3660
lodepuly <admin@qq.com>
2023-03-31 15:20:40 +0800

    简化代码

--- Resource/Script/Cainiao/Cainiao_amdc_reject.js
+++ Resource/Script/Cainiao/Cainiao_amdc_reject.js
@@ -1,27 +1,18 @@
 /*
 引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amdc.js
 */
-// 2023-03-31 12:32
+// 2023-03-31 15:08
 
 const url = $request.url;
 const header = $request.headers;
-//console.log(JSON.stringify(header))
+let ua = header["User-Agent"] || header["user-agent"];
 
-if ("undefined" !== typeof $task) {
-  let ua = header["User-Agent"];
-  if (ua.includes("Cainiao")) {
+if (ua.includes("Cainiao")) {
+  if (typeof $task !== "undefined") {
     $done({ status: "HTTP/1.1 404 Not Found" });
   } else {
-    $done({});
+    $done();
   }
 } else {
-  var ua = header["user-agent"];//h2
-  if (typeof ua == "undefined") {
-    ua = header["User-Agent"];//h1.1
-  }
-  if (ua.includes("AMap")) {
-    $done();
-  } else {
-    $done({});
-  }
+  $done({});
 }
