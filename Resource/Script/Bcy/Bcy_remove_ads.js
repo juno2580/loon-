614c91aef5567fdc52ae6d72285b22e6eb122829
lodepuly <admin@qq.com>
2023-04-04 00:28:00 +0800

    解除禁止保存限制

--- Resource/Script/Bcy/Bcy_remove_ads.js
+++ Resource/Script/Bcy/Bcy_remove_ads.js
@@ -6,6 +6,7 @@ if (typeof $response == "undefined") {
 
 let bodyObj = JSON.parse($response.body);
 bodyObj.data.properties.water_mark = false;
+bodyObj.data.properties.forbidden_right_click = false;
 
 
 let multi = bodyObj.data.multi;
