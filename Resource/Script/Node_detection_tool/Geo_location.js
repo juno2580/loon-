8d89515bfd131ec6d5a0a4a02b8a84ca46f0c074
Yi Ke <pligau@outlook.com>
2023-04-05 04:09:58 +0800

    优化排版

--- Resource/Script/Node_detection_tool/Geo_location.js
+++ Resource/Script/Node_detection_tool/Geo_location.js
@@ -38,13 +38,13 @@ const paran = ["IP","ISP","地区","城市"];
 
 $httpClient.get(requestParams, (error, response, data) => {
     if (error) {
-        message = "</br></br>🛑 查询超时"
+        message = "</br></br>🔴 查询超时"
         message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">` + message + `</p>`
-        $done({"title": "🔎 IP.SB 查询结果", "htmlMessage": message});
+        $done({"title": "   地理位置查询结果", "htmlMessage": message});
     } else {
         console.log(data);
         message = data ? json2info(data, paras) : "";
-        $done({"title": "    地理位置查询结果", "htmlMessage": message});
+        $done({"title": "   地理位置查询结果", "htmlMessage": message});
     }
 })
 
