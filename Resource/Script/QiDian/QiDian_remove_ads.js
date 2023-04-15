cdd89b0191a0b3e3810cb4a7b29ada1875ba3d36
lodepuly <admin@qq.com>
2023-04-16 01:20:18 +0800

    更新起点读书去广告

--- Resource/Script/QiDian/QiDian_remove_ads.js
+++ Resource/Script/QiDian/QiDian_remove_ads.js
@@ -1,70 +1,146 @@
 /*
-脚本引用https://raw.githubusercontent.com/app2smile/rules/master/js/adsense.js
+脚本引用https://raw.githubusercontent.com/app2smile/rules/master/js/qidian.js
 */
-const url = $request.url;
-const method = $request.method;
+let url = $request.url;
+let method = $request.method;
 if (!$response.body) {
-    // 穿山甲有概率会有此情况
-    console.log('$response.body为空');
+    console.log(`$response.body为undefined:${url}`);
     $done({});
 }
+
 let body = JSON.parse($response.body);
 
+const noticeTitle = "起点App脚本错误";
 const getMethod = "GET";
 const postMethod = "POST";
-const noticeTitle = "广告联盟";
 
-if ((url.includes("api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk")
-        || url.includes("is.snssdk.com/api/ad/union/sdk"))
-    && method === postMethod) {
-    console.log('穿山甲-get_ads');
-    if (!body.message) {
-        console.log(`body:${$response.body}`);
-        // 错误码 https://www.csjplatform.com/supportcenter/5421
-        if (!('status_code' in body)) {
-            $notification.post(noticeTitle, "穿山甲", "message/status_code字段错误");
-        } else {
-            console.log('广告为空');
-        }
-    } else {
-        console.log(Object.keys(body));
-        body = {
-            "request_id": 'F5617E54-3FF4-4052-9B09-4227D09B5105',
-            "status_code": 20001,
-            "reason": 112,
-            "desc": "该代码位请求量过大且消耗过低，因此填充率控制在10%以内，该策略每日生效，如果当天该代码位的消耗上涨或请求量小于5000，则次日不会命中该策略"
-        };
-        console.log('成功');
-    }
-} else if (url.includes('mi.gdt.qq.com') && method === getMethod) {
-    console.log('优量汇');
-    if ('ret' in body) {
-        if (body.ret === 0) {
-            // https://developers.adnet.qq.com/doc/android/union/union_debug#sdk%20%E9%94%99%E8%AF%AF%E7%A0%81
-            body.ret = 102006;
-            console.log('修改ret成功');
-        } else {
-            console.log(`ret不为0,不处理`);
-        }
-    } else {
-        console.log(`body:${$response.body}`);
-        $notification.post(noticeTitle, "优量汇", "无ret");
-    }
-} else if (url.includes('open.e.kuaishou.com') && method === postMethod) {
-    console.log('快手联盟');
-    if (body.result === 1) {
-        // 错误码: https://u.kuaishou.com/home/detail/1158
-        body.result = 40003;
-        console.log('修改result成功');
-    } else {
-        console.log('无需修改result');
-    }
+if (!body.Data) {
+    console.log(`body:${$response.body}`);
+    $notification.post(noticeTitle, "起点", "Data为空");
 } else {
-    $notification.post(noticeTitle, "路径/请求方法匹配错误:", method + "," + url);
-}
+    if (url.includes("v4/client/getsplashscreen") && method === getMethod) {
+        console.log('起点-开屏页');
+        if (!body.Data.List) {
+            console.log(`body:${$response.body}`);
+            $notification.post(noticeTitle, "起点", "List字段空");
+        } else {
+            body.Data.List = null;
+            console.log('List成功');
+        }
+        if ('EnableGDT' in body.Data) {
+            if (body.Data.EnableGDT === 1) {
+                body.Data.EnableGDT = 0;
+                console.log('EnableGDT成功');
+            } else {
+                console.log('无需修改EnableGDT');
+            }
+        } else {
+            console.log(`body:${$response.body}`);
+            $notification.post(noticeTitle, "起点", "EnableGDT字段为空");
+        }
 
+    } else if (url.includes("v2/deeplink/geturl") && method === getMethod) {
+        console.log('起点-不跳转精选页');
+        if (body.Data.ActionUrl === 'QDReader://Bookstore') {
+            body.Data = null;
+            console.log('成功');
+        } else {
+            console.log('无需处理,body:' + $response.body);
+        }
+    } else if (url.includes("v1/adv/getadvlistbatch?positions=iOS_tab") && method === getMethod) {
+        console.log('起点-iOS_tab');
+        if (!body.Data.iOS_tab) {
+            console.log(`body:${$response.body}`);
+            $notification.post(noticeTitle, "起点-iOS_tab", "iOS_tab字段为空");
+        } else {
+            if (body.Data.iOS_tab.length === 0) {
+                console.log('返回配置空');
+            } else {
+                body.Data.iOS_tab = [];
+                console.log('成功');
+            }
+        }
+    } else if (url.includes("v1/dailyrecommend/getdailyrecommend") && method === getMethod) {
+        // 需全新应用
+        console.log('起点-每日导读');
+        if (body.Data.length) {
+            body.Data = [];
+            console.log('成功');
+        } else {
+            console.log('每日导读无数据');
+        }
+    } else if (url.includes("v1/bookshelf/getHoverAdv") && method === getMethod) {
+        console.log('起点-书架悬浮广告');
+        if (body.Data.ItemList?.length) {
+            console.log('成功' + body.Data.ItemList.length);
+            body.Data.ItemList = [];
+        } else {
+            console.log('无需处理');
+        }
+    } else if (url.includes("v1/client/getconf") && method === postMethod) {
+        console.log('起点-getconf');
+        // 精选 和 发现 中间的活动配置
+        if (!body.Data.ActivityPopup?.Data) {
+            console.log(`body:${$response.body}`);
+            $notification.post(noticeTitle, "起点-getconf", "ActivityPopup/Data字段为空");
+        } else {
+            body.Data.ActivityPopup = null;
+            console.log('ActivityPopup(活动弹窗)成功');
+        }
+        if (body.Data.WolfEye === 1) {
+            // 使5.9.196版本 tcp强制走https
+            console.log('WolfEye修改为0');
+            body.Data.WolfEye = 0;
+        } else {
+            console.log(`无需修改WolfEye:${body.Data.WolfEye}`);
+        }
+
+        // QDReader://Bookshelf 书架右下角悬浮活动
+        if (body.Data.ActivityIcon?.Type !== 0) {
+            console.log(`body:${$response.body}`);
+            $notification.post(noticeTitle, "起点-getconf", "ActivityIcon/Type字段错误");
+        } else {
+            // 无活动icon的情况下为{"EndTime":0,"StartTime":0,"Type":0}
+            if (body.Data.ActivityIcon.EndTime === 0) {
+                console.log('无ActivityIcon配置');
+            } else {
+                body.Data.ActivityIcon.StartTime = 0;
+                body.Data.ActivityIcon.EndTime = 0;
+                delete body.Data.ActivityIcon.Actionurl;
+                delete body.Data.ActivityIcon.Icon;
+                console.log('ActivityIcon成功');
+            }
+        }
+
+        // 功能增强:搜索页可以搜索用户
+        if (body.Data.EnableSearchUser === "1") {
+            console.log(`无需修改搜索用户配置`);
+        } else {
+            body.Data.EnableSearchUser = "1";
+            console.log(`允许搜索用户成功:${body.Data.EnableSearchUser}`);
+        }
+
+        // if (body.Data.hasOwnProperty('EnableClipboardReading')) {
+        //     if (body.Data.EnableClipboardReading === 1) {
+        //         body.Data.EnableClipboardReading = 0;
+        //         console.log('不允许读取剪切板');
+        //     } else {
+        //         console.log('无需修改剪切板配置');
+        //     }
+        // } else {
+        //     console.log("body:" + $response.body);
+        //     $notification.post(notifiTitle, "起点-getconf", "EnableClipboardReading字段错误");
+        // }
+        // QDReader://UserCenter   我
+        // QDReader://Bookshelf    书架
+        // QDReader://Bookstore    精选
+    } else {
+        $notification.post(noticeTitle, "起点App路径/请求方法匹配错误:", method + "," + url);
+    }
+}
 body = JSON.stringify(body);
 
 $done({
     body
 });
+
